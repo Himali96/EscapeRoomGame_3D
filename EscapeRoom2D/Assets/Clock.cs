042537c0ed01ee0769a2e11e74aabbc345aa91db
divyesh155 <d3v.p8l@gmail.com>
2023-03-16 15:59:58 -0400

    added Clock Mini Game
    Clicking on clock to change time and set the time to 4:55 to win the game.

---
+++ EscapeRoom2D/Assets/Clock.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField]
+    private Transform minuteHand, hourHand;
+
+    [SerializeField]
+    private GameObject winText;
+
+    private void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        minuteHand.Rotate(Vector3.back, 30);
+        hourHand.Rotate(Vector3.back, 2.5f);
+
+        if ((Mathf.Round(minuteHand.rotation.eulerAngles.z * 2) / 2) == 30
+            && (Mathf.Round(hourHand.rotation.eulerAngles.z * 2) / 2) == 212.5f)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
