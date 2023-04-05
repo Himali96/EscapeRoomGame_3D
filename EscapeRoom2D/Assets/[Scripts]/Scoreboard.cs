e05319f375334f3f24c721e922ff9458b164e2c1
mitm800 <mitm800@gmail.com>
2023-04-05 01:18:23 -0400

    Updated Scoreboard scene

--- EscapeRoom2D/Assets/[Scripts]/Scoreboard.cs
+++ EscapeRoom2D/Assets/[Scripts]/Scoreboard.cs
@@ -14,18 +14,22 @@ public class Scoreboard : MonoBehaviour
         // Get the time taken for each level
         float level1Time = GameManager.instance.GetLevelTime(1);
         float level2Time = GameManager.instance.GetLevelTime(2);
+        float level3Time = GameManager.instance.GetLevelTime(3);
 
         // Display the time taken for each level
         timerText.SetText("Level 1 Time: " + Mathf.RoundToInt(level1Time) + "s\n\n" +
-                          "Level 2 Time: " + Mathf.RoundToInt(level2Time) + "s");
+                          "Level 2 Time: " + Mathf.RoundToInt(level2Time) + "s\n\n" +
+                          "Level 3 Time: " + Mathf.RoundToInt(level3Time) +  "s");
 
         // Determine the number of stars for each level based on the time taken
         int level1Stars = CalculateStars(level1Time);
         int level2Stars = CalculateStars(level2Time);
+        int level3Stars = CalculateStars(level3Time);
 
         // Display the appropriate number of stars for each level
         DisplayStars(starContainers[0], level1Stars);
         DisplayStars2(starContainers[1], level2Stars);
+        DisplayStars3(starContainers[1], level3Stars);
     }
 
     // Calculates the number of stars based on the time taken
@@ -56,7 +60,7 @@ public class Scoreboard : MonoBehaviour
 
             // Set the position of the star object based on its index in the container
             starObject.transform.SetParent(starContainer.transform);
-            starObject.transform.localPosition = new Vector3(i + 2.25f, 0.05f, 0);
+            starObject.transform.localPosition = new Vector3(i + 2.25f, 1.6f, 0);
             starObject.transform.localScale = new Vector3(0.04f, 0.04f, 1f);
         }
     }
@@ -72,7 +76,23 @@ public class Scoreboard : MonoBehaviour
 
             // Set the position of the star object based on its index in the container
             starObject.transform.SetParent(starContainer.transform);
-            starObject.transform.localPosition = new Vector3(i + 2.25f, -1.65f, 0);
+            starObject.transform.localPosition = new Vector3(i + 2.25f, -0.1f, 0);
+            starObject.transform.localScale = new Vector3(0.04f, 0.04f, 1f);
+        }
+    }
+
+    private void DisplayStars3(GameObject starContainer, int numStars)
+    {
+        for (int i = 0; i < numStars; i++)
+        {
+            // Create a new sprite object for each star and set its sprite to the star sprite
+            GameObject starObject = new GameObject("Star");
+            SpriteRenderer renderer = starObject.AddComponent<SpriteRenderer>();
+            renderer.sprite = starSprite;
+
+            // Set the position of the star object based on its index in the container
+            starObject.transform.SetParent(starContainer.transform);
+            starObject.transform.localPosition = new Vector3(i + 2.25f, -1.84f, 0);
             starObject.transform.localScale = new Vector3(0.04f, 0.04f, 1f);
         }
     }
